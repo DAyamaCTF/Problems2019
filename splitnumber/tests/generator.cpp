dc7dddf60f0703c7a3747ade997460e548079718
emtsubasa <yagimeemeesann@gmail.com>
2019-04-27 02:05:39 +0900

    add samplecase

--- splitnumber/tests/generator.cpp
+++ splitnumber/tests/generator.cpp
@@ -9,7 +9,7 @@ int main(int argc, char* argv[]) {
   registerGen(argc, argv, 1);
   generate_random("small.in",MINT,10,MINP,10);
   generate_random("med.in",MINT,MAXT,MINP,100);
-  generate_random("large.in",MINT,MAXT,MINP,MAXP);
+  generate_random("large.in",MAXT,MAXT,MINP,MAXP);
   return 0;
 }
 
@@ -18,6 +18,7 @@ void generate_random(string filename, int mint, int maxt, int minp, int maxp){
      ofstream ofs(filename);
      int t = rnd.next(mint,maxt);
      ofs << t << endl;
-     for(int i=0;i<t;++i)ofs << rnd.next(minp,maxp) << endl;
+     for(int i=1;i<t;++i)ofs << rnd.next(minp,maxp) << endl;
+     ofs << maxp << endl;
      ofs.close();
 }
