5c65e43a9b81922fbf4580d942ba24949d3c5b0d
DAyama <taiki_yamada@akane.waseda.jp>
2019-05-08 11:08:40 +0900

    modify statement and constrains

---
+++ explode/dayama-c/main.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <string.h>
+
+int H,W;
+int di[] = {0,-1,0,+1};
+int dj[] = {+1,0,-1,0};
+int reachable[50][50];
+char map[50][50];
+
+int isvalid(int i, int j){
+  return (i >= 0 && i < H && j >= 0 && j < W ? 1 : 0);
+}
+
+int dfs(int i, int j){
+  if(reachable[i][j] != -1){
+    return reachable[i][j];
+  }
+  reachable[i][j] = 2; // visited
+
+  int size = map[i][j] - '0';
+  int result = 0, dir;
+  for(dir = 0; dir < 4; dir++){
+    int len = 1;
+    int ni = i + di[dir], nj = j + dj[dir];
+    while(len <= size && isvalid(ni, nj) == 1 && map[ni][nj] != '#'){
+      if((isdigit(map[ni][nj]) && reachable[ni][ni] == -1 && dfs(ni, nj) == 1) || map[ni][nj] == 'W'){
+        result = 1;
+      }
+
+      ni += di[dir]; nj += dj[dir];
+      len++;
+    }
+  }
+
+  return reachable[i][j] = result;
+}
+
+int main(){
+  int T;
+  scanf("%d", &T);
+  while(T--){
+    scanf("%d %d", &H, &W);
+
+    int i, j;
+    for(i = 0; i < H; i++){
+      scanf("%s", map[i]);
+    }
+
+    int ans = 0;
+
+    for(i = 0; i < H; i++){
+      for(j = 0; j < W; j++){
+        memset(reachable, -1, sizeof(reachable));
+
+        if(!isdigit(map[i][j])) continue;
+        if(dfs(i, j) == 1) ans++;
+      }
+    }
+    
+    printf("%d\n", ans);
+  }
+}
--- explode/tests/constraints.h
+++ explode/tests/constraints.h
@@ -2,7 +2,7 @@
 #define MAX_T_SMALL 10
 #define MIN_T_LARGE 50
 #define MAX_T_LARGE 50
-#define MIN_HW_SMALL 4
-#define MAX_HW_SMALL 5
+#define MIN_HW_SMALL 1
+#define MAX_HW_SMALL 10
 #define MIN_HW_LARGE 45
 #define MAX_HW_LARGE 50
