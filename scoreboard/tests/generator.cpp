41daf509df5351274421b6164b3bb065590160f3
DAyama <taiki_yamada@akane.waseda.jp>
2019-05-10 12:35:57 +0900

    scoreboard: modify generator

--- scoreboard/tests/generator.cpp
+++ scoreboard/tests/generator.cpp
@@ -7,17 +7,37 @@ using namespace std;
 
 void generate(const string &file_name, int num_case, int min_num, int max_num) {
   ofstream ofs(file_name);
-  ofs << num_case <<endl;
+  ofs << num_case << endl;
 
   if(file_name == "small.in"){ // small
     for (int i = 0; i < num_case; i++) {
-      ofs << rnd.next(min_num, max_num) << " ";
-      ofs << rnd.next(min_num, max_num) << endl;
+      int status = rnd.next(0,2);
+      int a = rnd.next(min_num, max_num);
+      int b = rnd.next(min_num, max_num);
+      if(a > b) swap(a, b);
+
+      if(status == 0){ // Win
+        ofs << a << " " << b << endl;
+      }else if(status == 1){ // Lose
+        ofs << b << " " << a << endl;
+      }else if(status == 2){ // Draw
+        ofs << a << " " << a << endl;
+      }
     }
   }else{ // large
     for (int i = 0; i < num_case - 2; i++) {
-      ofs << rnd.next(min_num, max_num) << " ";
-      ofs << rnd.next(min_num, max_num) << endl;
+      int status = rnd.next(0,2);
+      int a = rnd.next(min_num, max_num);
+      int b = rnd.next(min_num, max_num);
+      if(a > b) swap(a, b);
+
+      if(status == 0){ // Win
+        ofs << a << " " << b << endl;
+      }else if(status == 1){ // Lose
+        ofs << b << " " << a << endl;
+      }else if(status == 2){ // Draw
+        ofs << a << " " << a << endl;
+      }
     }
     ofs << 33 << " " << 4 << endl; // Marines
     ofs << 4 << " " << 33 << endl; // Tigers
